6fe862458d534039b6025818a9b619748b922db2
nelli gorlova <nellydev@quadrantcapital.io>
2025-10-15 23:39:43 +0600

    Changed the name of the buttons

--- frontend/src/pages/MainPage.tsx
+++ frontend/src/pages/MainPage.tsx
@@ -4,13 +4,13 @@ import { useNavigate } from 'react-router-dom';
 const MainPage = () => {
   const navigate = useNavigate();
 
-  const toCandle = () => navigate('/candle');
+  const toCandle = () => navigate('/candlestick');
   const toDepth = () => navigate('/order');
 
   return (
     <HStack justify="center" minH="100vh">
-      <Button w="200px" h="200px" fontSize="20px" onClick={toCandle}>Candlestick Chart</Button>
-      <Button w="200px" h="200px" fontSize="20px" onClick={toDepth}>Depth Chart</Button>
+      <Button w="200px" h="200px" fontSize="20px" onClick={toCandle}>Candlestick</Button>
+      <Button w="200px" h="200px" fontSize="20px" onClick={toDepth}>Order book</Button>
     </HStack>
   );
 };
